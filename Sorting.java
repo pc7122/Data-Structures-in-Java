3f5f8b7b16f1bbe1386972805c69003f086a920b
Prathamesh Chaudhary <prathameshchaudhary7122@gmail.com>
2023-09-19 13:04:17 +0530

    Sorting

---
+++ Sorting.java
@@ -0,0 +1,52 @@
+class Sorting {
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[min] > arr[j])
+                    min = j;
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int j = i - 1;
+            int key = arr[i];
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void showArray(int[] arr) {
+        System.out.println("Elements of array: ");
+        for (int j : arr) {
+            System.out.print(j + "\t");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 11, 33, 7, 2, 5, 8, 12, 45, 3 };
+        insertionSort(arr);
+        showArray(arr);
+    }
+}
+
